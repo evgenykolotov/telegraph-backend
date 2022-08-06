f903ca496e48b56cc5e11791502e31b8b7419d9e
Evgeny Kolotov <evgenykolotov.it@gmail.com>
2022-08-07 00:03:09 +0300

    Удалены неиспользуемые файлы от nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -9,7 +7,5 @@ import { AppService } from './app.service';
       envFilePath: `environment/.${process.env.NODE_ENV}.env`,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
