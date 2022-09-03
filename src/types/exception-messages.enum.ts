a4c9a88f1103cb4c378f6fd0988050baeac63ac5
Evgeny Kolotov <kolotov@wiseadvice.ru>
2022-09-03 23:35:49 +0300

    Добавлено перечисление для ошибок приложения

---
+++ src/types/exception-messages.enum.ts
@@ -0,0 +1,5 @@
+export const enum EXCEPTION_MESSAGES {
+  USER_NOT_FOUND = 'Пользователь с таким email не найден',
+  FOUND_CANDIDATE = 'Пользователь уже существует',
+  WRONG_AUTH_DATA = 'Не верный логин или пароль',
+}
