96b7ed574ef1d70a2e9e23c6b37c4597278076b7
Evgeny Kolotov <kolotov@wiseadvice.ru>
2022-09-08 22:38:49 +0300

    Добавлен роут для авторизации пользователя

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -6,10 +6,16 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Response } from 'express';
 import { CreateUserDTO } from '../users/dto/create-user.dto';
 import { AuthDataDTO } from './dto/auth-data.dto';
+import { LoginUserDTO } from './dto/login-user.dto';
 import { AuthService } from './services/auth.service';
 
 /**
@@ -50,6 +56,32 @@ export class AuthController {
     @Body() userDto: CreateUserDTO,
   ): Promise<AuthDataDTO> {
     const responseData = await this.authService.registration(userDto);
+    response.cookie('refresh_token', responseData.refresh_token, {
+      maxAge: AuthController.cookieMaxAge,
+      httpOnly: true,
+    });
+    return responseData;
+  }
+
+  /**
+   * Обработчик маршрута для авторизации пользователя.
+   * @public
+   * @param {Response} response - Объект ответа.
+   * @param {LoginUserDTO} userDto - Тело запроса на авторизацию пользователя.
+   * @returns {AuthDataDTO} - Данные с авторизацией пользователя.
+   */
+  @ApiOperation({ summary: 'Авторизация пользователя' })
+  @ApiOkResponse({
+    type: AuthDataDTO,
+    description: 'Авторизация пользователя прошла успешно',
+  })
+  @UsePipes(ValidationPipe)
+  @Post('/login')
+  public async login(
+    @Res({ passthrough: true }) response: Response,
+    @Body() userDto: LoginUserDTO,
+  ): Promise<AuthDataDTO> {
+    const responseData = await this.authService.login(userDto);
     response.cookie('refresh_token', responseData.refresh_token, {
       maxAge: AuthController.cookieMaxAge,
       httpOnly: true,
