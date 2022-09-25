f8b7365a2dce8631d582061bb09ffbe1a418ed2a
Evgeny Kolotov <kolotov@wiseadvice.ru>
2022-09-25 14:35:31 +0300

    Добавлена возможность авторизации по токену в спецификации Swagger

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import { RequestWithUser } from 'src/types/application.types';
-import JwtAuthenticationGuard from '../auth/guards/jwt-authentication.guard';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiOkResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { UserInfoDTO } from './dto/user-info.dto';
 import { UsersService } from './services/users.service';
+import { RequestWithUser } from 'src/types/application.types';
+import JwtAuthenticationGuard from '../auth/guards/jwt-authentication.guard';
 
 /**
  * @class
@@ -26,6 +31,7 @@ export class UsersController {
    * @returns {UserInfoDTO} - Информация пользователе.
    */
   @ApiOperation({ summary: 'Получение информации о пользователе' })
+  @ApiBearerAuth('JWT-auth')
   @ApiOkResponse({ type: UserInfoDTO, description: 'Даныые успешно получены' })
   @Get()
   @UseGuards(JwtAuthenticationGuard)
